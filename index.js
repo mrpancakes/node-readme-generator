065866008cd3f4514a970e25c10e54f0b38dd67c
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-04-04 16:51:35 -0500

    Installed inquirer and fs, added index.js file.

---
+++ index.js
@@ -0,0 +1,43 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = (answers) =>
+  `This is a markdown file`;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ])
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
+    fs.writeFile('readme.md', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created readme.md!')
+    );
+  });
